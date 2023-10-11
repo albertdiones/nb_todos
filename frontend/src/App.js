a66e6a27df55f5db9ffd70ba795d04b030c7fa35
Albert Diones <albert@addteam.org>
2023-10-11 11:29:24 +0800

    ItemList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
+
+class ItemList extends Component {
+  render() {
+    return <ol>
+      <li><input type={"checkbox"}/>Buy Potatoes</li>
+      <li><input type={"checkbox"}/>Buy Tomatoes</li>
+      <li><input type={"checkbox"}/>Buy Cucumber</li>
+      <li><input type={"checkbox"}/>Buy Lettuce</li>
+    </ol>;
+  }
+}
 
 function App() {
   return (
       <>
-    <header>
-      TODO List
-    </header>
-    <main>
-      <ol>
-        <li><input type={'checkbox'} />Buy Potatoes</li>
-        <li><input type={'checkbox'} />Buy Tomatoes</li>
-        <li><input type={'checkbox'} />Buy Cucumber</li>
-        <li><input type={'checkbox'} />Buy Lettuce</li>
-      </ol>
-    </main>
-    </>
+        <header>
+          TODO List
+        </header>
+        <main>
+          <ItemList/>
+        </main>
+      </>
   );
 }
 
