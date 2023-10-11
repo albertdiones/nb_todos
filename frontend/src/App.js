cd2231fc2f6d4362dd1441b29fd5e2daa0b73639
Albert Diones <albert@addteam.org>
2023-10-11 11:28:10 +0800

    frontend (react) setup

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <>
+    <header>
+      TODO List
+    </header>
+    <main>
+      <ol>
+        <li><input type={'checkbox'} />Buy Potatoes</li>
+        <li><input type={'checkbox'} />Buy Tomatoes</li>
+        <li><input type={'checkbox'} />Buy Cucumber</li>
+        <li><input type={'checkbox'} />Buy Lettuce</li>
+      </ol>
+    </main>
+    </>
+  );
+}
+
+export default App;
