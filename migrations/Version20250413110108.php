98e0e295c4eb5be228d5e222c15bb62fa7c6d28c
Albert Diones <adiones@addteam.org>
2025-04-13 19:22:17 +0800

    fixed the sequence of the migration

--- migrations/Version20250413110108.php
+++ migrations/Version20250413110108.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231010120825 extends AbstractMigration
+final class Version20250413110108 extends AbstractMigration
 {
     public function getDescription(): string
     {
